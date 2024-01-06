7e57ddb49c7ba924f23179d1c2b3d437a961f637
Russ <72242656+Russ741@users.noreply.github.com>
2024-01-06 00:13:38 -0500

    Add a second circle.

--- sketch.js
+++ sketch.js
@@ -11,6 +11,8 @@ function draw() {
     translate(width/2, height/2);
     fill(0);
     circle(0, 0, 0.75 * height);
+    fill(255);
+    circle(0, 0, 0.375 * height);
 }
 
 // Don't need to call new js(); here - https://github.com/processing/p5.js/issues/4985
