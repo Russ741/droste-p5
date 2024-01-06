528f612dff34a1df72c0263f10fea2c1efe59251
Russ <72242656+Russ741@users.noreply.github.com>
2024-01-06 00:46:38 -0500

    Demonstrate that draw() doesn't start with a blank slate.

--- sketch.js
+++ sketch.js
@@ -22,8 +22,12 @@ function draw() {
     translate(width/2, height/2);
     time = millis();
 
-    zoom = (time / 3000) ** 2;
-    drawRecursive(height * zoom);
+    // zoom = (time / 3000) ** 2;
+    // drawRecursive(height * zoom);
+    stroke(0);
+    fill(0);
+
+    point(random(-width/2, width/2), random(-height/2, height/2));
 }
 
 // Don't need to call new js(); here - https://github.com/processing/p5.js/issues/4985
