adabca6d87b64da8893e5560f405eaa4a028ce3c
Russ <72242656+Russ741@users.noreply.github.com>
2024-01-06 01:35:41 -0500

    Modify zoom calculation to avoid drawing invisible (large) circles.

--- sketch.js
+++ sketch.js
@@ -5,8 +5,10 @@ function setup() {
     startMillis = millis();
 }
 
+const recurseMillis = 2000;
+const recurseRatio = 0.1875;
+
 function drawRecursive(d) {
-    // TODO: Skip drawing circles that are too big, too.
     if (d < 0.25) {
         // Too small to be worth drawing; stop
         return;
@@ -15,7 +17,7 @@ function drawRecursive(d) {
     circle(0, 0, 0.75 * d);
     fill(255);
     circle(0, 0, 0.375 * d);
-    drawRecursive(0.1875 * d);
+    drawRecursive(recurseRatio * d);
 }
 
 function draw() {
@@ -23,8 +25,9 @@ function draw() {
     translate(width/2, height/2);
     const elapsedMillis = millis() - startMillis;
 
-    const zoom = 2 ** (elapsedMillis / 1000);
-    drawRecursive(height * zoom);
+    const loopProgress = elapsedMillis % recurseMillis / recurseMillis;
+    const zoom = (1 / recurseRatio) ** (loopProgress);
+    drawRecursive(2 * height * zoom);
 }
 
 // Don't need to call new js(); here - https://github.com/processing/p5.js/issues/4985
