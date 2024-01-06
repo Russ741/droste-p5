9705faf56b2cc57a5dd0d7e6f869e554d4b898de
Russ <72242656+Russ741@users.noreply.github.com>
2024-01-06 00:17:25 -0500

    Zoom in.

--- sketch.js
+++ sketch.js
@@ -2,17 +2,20 @@ function setup() {
     width = 500;
     height = 500;
     createCanvas(width, height);
-
+    startTime = millis();
 
     loop();
 }
 
 function draw() {
     translate(width/2, height/2);
+    time = millis();
+
+    zoom = (time / 3000) ** 2;
     fill(0);
-    circle(0, 0, 0.75 * height);
+    circle(0, 0, 0.75 * height * zoom);
     fill(255);
-    circle(0, 0, 0.375 * height);
+    circle(0, 0, 0.375 * height * zoom);
 }
 
 // Don't need to call new js(); here - https://github.com/processing/p5.js/issues/4985
