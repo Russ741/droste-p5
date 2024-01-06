1501448b52630b36c68c01c24310d85628de27af
Russ <72242656+Russ741@users.noreply.github.com>
2024-01-06 00:26:03 -0500

    First attempt at recursion (slows way down).

--- sketch.js
+++ sketch.js
@@ -7,15 +7,25 @@ function setup() {
     loop();
 }
 
+function drawRecursive(r) {
+    // TODO: Skip drawing circles that are too big, too.
+    if (r < 0.25) {
+        // Too small to be worth drawing; stop
+        return;
+    }
+    fill(0);
+    circle(0, 0, 0.75 * r);
+    fill(255);
+    circle(0, 0, 0.375 * r);
+    drawRecursive(0.1875 * r);
+}
+
 function draw() {
     translate(width/2, height/2);
     time = millis();
 
     zoom = (time / 3000) ** 2;
-    fill(0);
-    circle(0, 0, 0.75 * height * zoom);
-    fill(255);
-    circle(0, 0, 0.375 * height * zoom);
+    drawRecursive(height * zoom);
 }
 
 // Don't need to call new js(); here - https://github.com/processing/p5.js/issues/4985
