0ae736b5fc5f1bafcdad9019819d2c25b31031c4
Russ <72242656+Russ741@users.noreply.github.com>
2024-01-06 03:13:42 -0500

    Allow for recursion for non-centered shapes.

--- sketch.js
+++ sketch.js
@@ -11,11 +11,15 @@ class Shape {
         this.color = color;
     }
 }
+const hs2 = 0.5/Math.sqrt(2);
 
+// TODO: Specify shapes and recursions separately.
 const shapes = [
     new Shape(0, 0, 1, "black"),
-    new Shape(0, 0, .5, "white"),
-    new Shape(0, 0, .25, "black")
+    new Shape(-hs2, hs2, .5, "white"),
+    new Shape(hs2, -hs2, 0.5, "white"),
+    new Shape(hs2, -hs2, .25, "black"),
+    new Shape(-hs2, hs2, .25, "black")
 ];
 
 const recurseMillis = 2000;
@@ -29,16 +33,19 @@ function setup() {
     noStroke();
 }
 
-function drawRecursive(r) {
+function drawRecursive(r, x, y) {
     if (r < 0.25) {
         // Too small to be worth drawing; stop
         return;
     }
     for (const shape of shapes) {
         fill(shape.color);
-        circle(shape.relX * r, shape.relY * r, 2 * shape.relR * r);
+        circle(shape.relX * r + x, shape.relY * r + y, 2 * shape.relR * r);
     }
-    drawRecursive(recurseRatio * r);
+    const recurseShape = shapes.slice(-1)[0];
+    const nextX = r * recurseShape.relX + x;
+    const nextY = r * recurseShape.relY + y;
+    drawRecursive(recurseRatio * r, nextX, nextY);
 }
 
 function draw() {
@@ -48,7 +55,16 @@ function draw() {
 
     const loopProgress = elapsedMillis % recurseMillis / recurseMillis;
     const zoom = (1 / recurseRatio) ** (loopProgress);
-    drawRecursive(height * zoom);
+    const r = height / 2 * zoom;
+
+    const recurseShape = shapes.slice(-1)[0];
+    // Formula for the nth term of a geometric series; this won't work for co-recursion, but it looks good for now.
+    const translationProgress = (1 - recurseRatio ** loopProgress) / (1 - recurseRatio);
+    const endX = -recurseShape.relX * r;
+    const curX = endX * translationProgress;
+    const endY = -recurseShape.relY * r;
+    const curY = endY * translationProgress;
+    drawRecursive(r, curX, curY);
 }
 
 // Don't need to call new js(); here - https://github.com/processing/p5.js/issues/4985
